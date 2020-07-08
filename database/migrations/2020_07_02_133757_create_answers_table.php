f9cd3bc2ae3c2996704690eafa923a496bb6f413
widdanbaehaqi <widdanbaehaqi@mhs.stmik-yadika.ac.id>
2020-07-08 19:16:07 +0700

    Make file migrations

---
+++ database/migrations/2020_07_02_133757_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longtext('isi')->nullable();
+            $table->timestamps();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('question_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
